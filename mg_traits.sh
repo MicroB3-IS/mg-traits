e8e02ab92aea95ebd34447363376d918e844f08a
pereiramemo <pereiramemo@gmail.com>
2016-07-27 11:45:48 +0200

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -39,11 +39,13 @@ SLV_FILE="${THIS_JOB_TMP_DIR}/data/silva_tax_order_115.txt"
 ###########################################################################################################
 
 # urldecode input
-string=$(echo "${1}" | sed -e 's/&/|/g' -e 's/\+/ /g' -e 's/%25/%/g' -e 's/%20/ /g' -e 's/%09/ /g' -e 's/%21/!/g' -e \
-'s/%22/"/g' -e 's/%23/#/g' -e 's/%24/\$/g' -e 's/%26/\&/g' -e 's/%27/'\''/g' -e 's/%28/(/g' -e 's/%29/)/g' -e \
-'s/%2a/\*/g' -e 's/%2b/+/g' -e 's/%2c/,/g' -e 's/%2d/-/g' -e 's/%2e/\./g' -e 's/%2f/\//g' -e 's/%3a/:/g' -e 's/%3b/;/g'\
--e 's/%3d/=/g' -e 's/%3e//g' -e 's/%3f/?/g' -e 's/%40/@/g' -e 's/%5b/\[/g' -e 's/%5c/\\/g' -e 's/%5d/\]/g' -e \
-'s/%5e/\^/g' -e 's/%5f/_/g' -e 's/%60/`/g' -e 's/%7b/{/g' -e 's/%7c/|/g' -e 's/%7d/}/g' -e 's/%7e/~/g' -e 's/%09/      /g')
+string=$(echo $1 | sed -e 's/&/|/g' -e 's/\+/ /g' -e 's/%25/%/g' -e 's/%20/ /g' -e 's/%09/ /g' -e 's/%21/!/g' -e 's/%22/"/g' -e 's/%23/#/g' -e 's/%24/\$/g' -e 's/%26/\&/g' -e 's/%27/'\''/g' -e 's/%28/(/g' -e 's/%29/)/g' -e 's/%2a/\*/g' -e 's/%2b/+/g' -e 's/%2c/,/g' -e 's/%2d/-/g' -e 's/%2e/\./g' -e 's/%2f/\//g' -e 's/%3a/:/g' -e 's/%3b/;/g' -e 's/%3d/=/g' -e 's/%3e//g' -e 's/%3f/?/g' -e 's/%40/@/g' -e 's/%5b/\[/g' -e 's/%5c/\\/g' -e 's/%5d/\]/g' -e 's/%5e/\^/g' -e 's/%5f/_/g' -e 's/%60/`/g' -e 's/%7b/{/g' -e 's/%7c/|/g' -e 's/%7d/}/g' -e 's/%7e/~/g' -e 's/%09/      /g')
+
+# string=$(echo "${1}" | sed -e 's/&/|/g' -e 's/\+/ /g' -e 's/%25/%/g' -e 's/%20/ /g' -e 's/%09/ /g' -e 's/%21/!/g' -e \
+# 's/%22/"/g' -e 's/%23/#/g' -e 's/%24/\$/g' -e 's/%26/\&/g' -e 's/%27/'\''/g' -e 's/%28/(/g' -e 's/%29/)/g' -e \
+# 's/%2a/\*/g' -e 's/%2b/+/g' -e 's/%2c/,/g' -e 's/%2d/-/g' -e 's/%2e/\./g' -e 's/%2f/\//g' -e 's/%3a/:/g' -e 's/%3b/;/g'\
+# -e 's/%3d/=/g' -e 's/%3e//g' -e 's/%3f/?/g' -e 's/%40/@/g' -e 's/%5b/\[/g' -e 's/%5c/\\/g' -e 's/%5d/\]/g' -e \
+# 's/%5e/\^/g' -e 's/%5f/_/g' -e 's/%60/`/g' -e 's/%7b/{/g' -e 's/%7c/|/g' -e 's/%7d/}/g' -e 's/%7e/~/g' -e 's/%09/      /g')
 
 # set delimiter
 IFS="|"
@@ -102,10 +104,6 @@ EOF
   exit 1
 fi
 
-mail -s "test flag" epereira@mpi-bremen.de << EOF
-"sample ${SAMPLE_LABEL}, mg_url ${MG_URL}, mg_id ${MG_ID}, job_id ${JOB_ID}"
-EOF
-
 ###########################################################################################################
 # 1 - Check database connection
 ###########################################################################################################
@@ -123,6 +121,11 @@ if [[ "${DB_RESULT}" != "UPDATE 1" ]]; then
   email_comm "sample name ${SAMPLE_LABEL} is not in database Result:${DB_RESULT}"  
   cleanup && exit 2
 fi
+
+
+mail -s "test flag" epereira@mpi-bremen.de << EOF
+"sample ${SAMPLE_LABEL}, mg_url ${MG_URL}, mg_id ${MG_ID}, job_id ${JOB_ID}, db_result ${DB_RESULT}"
+EOF
 
 ###########################################################################################################
 # 2 - Create job directory
