8ed0e923ccc87e4af3d2bdddb55201bb1fe3bdc2
pereiramemo <pereiramemo@gmail.com>
2016-09-29 16:54:30 +0200

    update

---
+++ test/command.sh
@@ -0,0 +1,1 @@
+ PS4='Line ${LINENO}: ' bash ../mg_traits.sh "sample_label=test_label&mg_url=$URL&customer=anonymus&sample_environment=marine&time_submitted=now&make_public=TRUE&keep_data=FALSE&id=123"
