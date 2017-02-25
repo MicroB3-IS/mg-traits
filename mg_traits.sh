eab83dfc847cc0ef3fa6c7545834beff9a5ec606
pereiramemo <pereiramemo@gmail.com>
2017-02-25 17:12:05 +0100

    update

--- mg_traits.sh
+++ mg_traits.sh
@@ -13,8 +13,6 @@ if [[ -z "${JOB_ID}" ]]; then
    JOB_ID="${RANDOM}"
 fi
 
-touch /bioinf/projects/megx/mg-traits/mg-traits_dev/test_run
-
 ################################################################################
 # 0 - Load general configuration
 ################################################################################
